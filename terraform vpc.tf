f67d0a5804cb33351e6e6862a2b5010a8acb9a18
Ranjeetsheelvant <107619970+Ranjeetsheelvant@users.noreply.github.com>
2022-08-23 10:15:02 +0530

    Create terraform vpc.tf

---
+++ terraform vpc.tf
@@ -0,0 +1,40 @@
+#
+# VPC Resources
+#  * VPC
+#  * Subnets
+#  * Internet Gateway
+#  * Route Table
+#
+
+resource "aws_vpc" "demo" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = tomap({
+    "Name"                                      = "terraform-eks-demo-node",
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared",
+  })
+}
+
+resource "aws_subnet" "demo" {
+  count = 2
+
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.demo.id
+
+  tags = tomap({
+    "Name"                                      = "terraform-eks-demo-node",
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared",
+  })
+}
+resource "aws_internet_gateway" "demo" {
+  vpc_id = aws_vpc.demo.id
+
+  tags = {
+    Name = "terraform-eks-demo"
+  }
+}
+
+resource "aws_route_table" "demo" {
+  vpc_id = aws_vpc.demo.id
